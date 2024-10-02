fab282e3eea83cdc0698929dfe30891277a4f6fc
MichDawa <125820798+MichDawa@users.noreply.github.com>
2024-10-02 10:05:00 +0800

    commit create components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoteListComponent } from './components/note-list/note-list.component';
+import { NoteDetailComponent } from './components/note-detail/note-detail.component';
+import { NoteFormComponent } from './components/note-form/note-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteListComponent,
+    NoteDetailComponent,
+    NoteFormComponent
   ],
   imports: [
     BrowserModule,
