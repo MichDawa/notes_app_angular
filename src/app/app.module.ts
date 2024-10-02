7e4003c56ac2bc8c72c65d2ed0f0a7f2e668304f
MichDawa <125820798+MichDawa@users.noreply.github.com>
2024-10-02 10:37:27 +0800

    commit creating services and configuring the routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoteListComponent } from './components/note-list/note-list.component';
-import { NoteDetailComponent } from './components/note-detail/note-detail.component';
-import { NoteFormComponent } from './components/note-form/note-form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NoteListComponent,
-    NoteDetailComponent,
-    NoteFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
